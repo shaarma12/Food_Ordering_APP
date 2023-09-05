76d14a6e9e6a19499baa666c5b14b09b629c0d5e
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-09-05 23:29:06 +0530

    Episode-3

--- index.js
+++ index.js
@@ -20,17 +20,59 @@ import ReactDOM from "react-dom/client";
 
 // for two different id's:-
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child-1" }, [
-    React.createElement("h1", {}, "h1 tag!"),
-    React.createElement("h2", {}, "h2 tag!"),
-  ]),
-  React.createElement("div", { id: "child-2" }, [
-    React.createElement("h1", {}, "h1 tag!"),
-    React.createElement("h2", {}, "h2 tag!"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child-1" }, [
+//     React.createElement("h1", {}, "h1 tag!"),
+//     React.createElement("h2", {}, "h2 tag!"),
+//   ]),
+//   React.createElement("div", { id: "child-2" }, [
+//     React.createElement("h1", {}, "h1 tag!"),
+//     React.createElement("h2", {}, "h2 tag!"),
+//   ]),
+// ]);
+
+// ----------------------------------------------------------------------------------------
+
+// episode - 3
+
+// Creating React Element using Core React.
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React Component Using Core React!"
+);
+
+// JSX
+
+const JSXHeading = <h1 id="heading">Namaste React from JSX 🚀!</h1>;
+
+// Functional Component
+
+const HeadingComponent = () => {
+  return <h1 id="heading">Namaste React Coming From Component ❤!</h1>;
+};
+
+// React Element
+const ele = <span>React Element in the React Element!</span>;
+const title = (
+  <>
+    <h1>Title which is a React Element!</h1>
+    {ele}
+    <HeadingComponent />
+  </>
+);
+
+// Functional Component without using return Statement
+const HeadingComp = () => (
+  <>
+    <h1>Namaste Component!</h1>
+    {title}
+    <HeadingComponent />
+    <HeadingComponent></HeadingComponent>
+    {HeadingComponent()}
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComp />);
