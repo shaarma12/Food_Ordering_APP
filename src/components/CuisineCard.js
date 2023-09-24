c21ad64a601a7a3e2807d2a13323bd45d7a8f4cf
Himanshu Sharma <himanshusharma1581@gmail.com>
2023-09-24 22:42:11 +0530

    Price issue Fixed

--- src/components/CuisineCard.js
+++ src/components/CuisineCard.js
@@ -2,13 +2,16 @@ import React from "react";
 import rup from "../../images/rup.png";
 import { CUIS_IMG } from "../utils/constant";
 const CuisineCard = ({ restro }) => {
-  const { name, price, description, imageId } = restro?.card?.info;
+  const { name, price, description, imageId, defaultPrice } =
+    restro?.card?.info;
   return (
     <div className="flex justify-between w-[46rem] border-b-[1.599px] border-gray-300 pb-10 mb-5">
       <div className="write">
         <p className="font-semibold text-lg text-gray-800">{name}</p>
         <div className="flex">
-          <p className="text-sm font-medium mb-4">&#8377; {price / 100}</p>
+          <p className="text-sm font-medium mb-4">
+            &#8377; {price ? price / 100 : defaultPrice / 100}
+          </p>
         </div>
 
         <div className="text-sm text-gray-400 pr-7">
