8a35299e2d680e5f93d6b95a5e4f95875f426371
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-10-01 18:02:31 +0530

    Shimmer Fixed

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,51 +2,51 @@ import React from "react";
 const Shimmer = () => {
   return (
     <>
-      <div className="shimmer">
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+      <div className="flex flex-wrap justify-center gap-[5%] m-[5vmax]">
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-lg flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
-        <div className="inner">
-          <div className="ou"></div>
-          <div className="in"></div>
-          <div className="in"></div>
+        <div className="bg-gray-200 w-[20%] h-[18vmax] mb-[5%] rounded-md flex flex-col items-center">
+          <div className="w-[80%] h-[50%] m-[5%] mt-6 rounded-md bg-slate-300"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
+          <div className="w-[60%] h-[5%] m-[5%] bg-slate-300 rounded-lg"></div>
         </div>
       </div>
     </>
