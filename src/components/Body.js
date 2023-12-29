a2cc83a790e1e94686ae99970e5baf4e5e54fe9d
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-12-29 19:28:09 +0530

    corousel checker after updating swiggy's API

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,7 +11,6 @@ import OfferCorousel from "./OfferCorousel";
 const Body = () => {
   // Using Context
   const { Login, setChanger } = useContext(UserContext);
-
   const [rest, setRest] = useState([]);
   const [text, setText] = useState("");
   const [gridImage, setGridImage] = useState(null);
@@ -25,16 +24,18 @@ const Body = () => {
     const response = await url.json();
 
     setRest(
-      response?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+      response?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
     setFilterlist(
-      response?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+      response?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
     setGridImage(response?.data);
+    // setCorousel(gridImage?.cards[0]?.card?.card?.header?.title="What's on your mind?");
   };
 
+  const CorouselChecker = gridImage?.cards[0]?.card?.card?.header?.title;
   // Calling High Order Component
 
   const DiscountBanner = DiscountInfo(Rescard);
@@ -73,27 +74,29 @@ const Body = () => {
             Search
           </button>
         </div>
-        <div className="flex flex-col w-[75rem]">
-          <h2 className="mt-6 text-2xl font-bold text-gray-900 mb-6 ml-3">
-            Best offers for you
-          </h2>
-          <div className="flex overflow-x-scroll scroll-smooth no-scrollbar gap-2 w-[75rem]">
-            <div className="flex -ml-[20rem]">
-              {gridImage?.cards[0]?.card?.card?.imageGridCards?.info.map(
-                (i) => {
-                  return <OfferCorousel key={i?.id} corouselData={i} />;
-                }
-              )}
+        {!CorouselChecker && (
+          <div className="flex flex-col w-[75rem]">
+            <h2 className="mt-6 text-2xl font-bold text-gray-900 mb-6 ml-3">
+              Best offers for you
+            </h2>
+            <div className="flex overflow-x-scroll scroll-smooth no-scrollbar gap-2 w-[75rem]">
+              <div className="flex -ml-[20rem]">
+                {gridImage?.cards[0]?.card?.card?.imageGridCards?.info.map(
+                  (i) => {
+                    return <OfferCorousel key={i?.id} corouselData={i} />;
+                  }
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        )}
         <div className="flex flex-col w-[75rem] mb-16">
           <h2 className="mt-6 text-2xl font-bold text-gray-900 mb-6 ml-3">
-            {gridImage?.cards[1]?.card?.card?.header?.title}
+            {gridImage?.cards[0]?.card?.card?.header?.title}
           </h2>
           <div className="flex overflow-x-scroll scroll-smooth no-scrollbar gap-2 w-[75rem]">
             <div className="flex -ml-20">
-              {gridImage?.cards[1]?.card?.card?.imageGridCards?.info.map(
+              {gridImage?.cards[0]?.card?.card?.imageGridCards?.info.map(
                 (i) => {
                   return <GridCards key={i?.id} gridImage={i} />;
                 }
