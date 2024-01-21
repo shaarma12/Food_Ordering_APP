d110749478c0aa70ee8b4f3766ca4953bb17d3f3
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-01-21 23:22:10 +0530

    on hover bill

---
+++ src/components/ItemBill.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import veg from "../../images/veg.png";
+import nonveg from "../../images/nonveg.png";
+
+const ItemBill = ({ name, isVeg, price, defaultPrice, count }) => {
+  const Price = (price ? price / 100 : defaultPrice / 100) * count;
+  return (
+    <div className="flex mt-2 mb-2">
+      {isVeg ? (
+        <img className="w-4 m-1" src={veg} alt="veg" />
+      ) : (
+        <img className="w-4 m-1" src={nonveg} alt="nonveg" />
+      )}
+      <p className="flex text-sm font-medium w-60">
+        {name.length > 22 ? name.substring(0, 22) + "..." : name}
+        <p className="mx-1">x</p>
+        <p>{count}</p>
+      </p>
+      <p className="text-sm font-medium  text-gray-500">₹{Price}</p>
+    </div>
+  );
+};
+
+export default ItemBill;
--- src/components/OnHoverBill.js
+++ src/components/OnHoverBill.js
@@ -1,34 +1,57 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import ItemBill from "./ItemBill";
 const OnHoverBill = () => {
-  const data = useSelector((store) => store.cart.item);
+  const data = useSelector((store) => store.cart.items);
   const resData = useSelector((store) => store.cart.restruantName);
   console.log(resData);
   return (
-    <div className="w-[23rem] h-56 p-7 text-black border-2 border-red-600 absolute top-32 right-48 bg-white">
-      <div className="flex">
+    <div className="w-[23rem] p-7 text-black border-t-2 border-red-500 absolute top-32 right-48 bg-white shadow-lg shadow-black">
+      <div className="flex pb-5 border-b border-black">
         <img
-          className="w-20"
+          className="w-20 h-20"
           alt="OnHoverBill restaurant Image"
           src={
             "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
             resData[0]?.cloudinaryImageId
           }
         />
-        <div className="-mt-1 ml-6">
+        <div className="ml-6">
           <div>
             <p className="font-medium text-lg">{resData[0]?.name}</p>
             <p className="-mt-1 font-medium text-sm text-gray-400">
               {resData[0]?.areaName}
             </p>
           </div>
-          <p className="mt-4 font-bold text-sm text-[#919fe4]">
+          <p className="mt-5 font-bold text-xs text-[#7082de]">
             VIEW FULL MENU
           </p>
         </div>
       </div>
-      <div></div>
-      <div></div>
+      <div className="mt-5 -ml-1 max-h-56 overflow-y-scroll border-b-2 border-dotted border-gray-400">
+        {data.map((i) => {
+          return (
+            <ItemBill
+              key={i?.card?.info?.id}
+              name={i?.card?.info?.name}
+              isVeg={i?.card?.info?.isVeg}
+              price={i?.card?.info?.price}
+              defaultPrice={i?.card?.info?.defaultPrice}
+              count={i?.count}
+            />
+          );
+        })}
+      </div>
+      <div className="my-4">
+        <div className="flex justify-between font-medium">
+          <p>Sub total</p>
+          <p className="mr-1">₹ {}</p>
+        </div>
+        <p className="-mt-1 text-xs text-gray-400">Extra charges may apply</p>
+      </div>
+      <button className="bg-orange-500 text-white font-bold py-3 w-[19.3rem] mt-1 mb-1">
+        CHECKOUT
+      </button>
     </div>
   );
 };
