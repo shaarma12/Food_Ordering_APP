d6fa078f680b08dc3a343dd0f1f59b2f53f66e9c
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-01-29 01:11:43 +0530

    Order Confirmation done

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Confirmation = () => {
   return (
-    <div className='h-[28rem]'>
+    <div className='h-[28rem] mt-20'>
+        <div className='flex'>
         <img src="https://media.istockphoto.com/id/496603666/vector/flat-icon-check.jpg?s=612x612&w=0&k=20&c=BMYf-7moOtevP8t46IjHHbxJ4x4I0ZoFReIp9ApXBqU="
-        className=""/>
-      <p>order confirm</p>
+        className="w-60 ml-[17rem]"/>
+        <p className='text-7xl mt-20 font-bold'>Order Confirmed!</p>
+        </div>
+      <p className='ml-[32.5rem] -mt-14 text-2xl font-bold text-gray-400 w-[35rem]'>Your order is confirmed, you will be notified by Email and Text very soon.</p>
     </div>
   )
 }
