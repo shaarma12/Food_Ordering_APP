adab3f481c80eb6f7e38be613f9ef5f64778ad5a
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-02-04 23:17:44 +0530

    .

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,4 +6,4 @@ export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.693473&lng=77.297535&restaurantId=";
 export const ABOUT_API = "https://api.github.com/users/shaarma12";
 export const RES_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.690200&lng=77.306793&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5220971&lng=77.2101534&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
